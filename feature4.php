2c237cb5a3727426a7668b982e084b32509fbbc0
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-03-27 14:37:44 +0100

    Create feature4.php

---
+++ feature4.php
@@ -0,0 +1,98 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="form__field">
+					<label for="Woonplaats">In welke stad woon je</label>
+					<input type="text" id="Woonplaats" name="woonplaats">
+				</div>
+
+<label for="keuzeVak">Design of development? </label>
+<select id="keuzevak" name="keuzevak">
+<option value="design">Design</option>
+<option value="development">Development</option>
+</select>
+
+<br>
+
+<label for="jaar">In welk jaar zit je? </label>
+<select id="jaar" name="jaar">
+<option value="eersteJaar">1IMD</option>
+<option value="tweedeJaar">2IMD</option>
+<option value="derdeJaar">3IMD</option>
+</select>
+
+<br>
+<br>
+<label for="jaar">Wat doe je graag in je vrije tijd? </label>
+<br>
+<label for="sporten">
+    <input type="checkbox" id="sporten">
+    Sporten
+</label><br/>
+<label for="Gamen">
+    <input type="checkbox" id="gamen">
+    Gamen
+</label><br/>
+<label for="Creatief">
+    <input type="checkbox" id="creatief">
+    Creatief bezig zijn 
+</label><br/>
+<label for="Feesten">
+    <input type="checkbox" id="feesten">
+    Feesten
+</label><br/>
+<label for="instrument">
+    <input type="checkbox" id="instrument">
+    Een instrument beoefenen
+</label><br/>
+<label for="andere">
+    <input type="checkbox" id="andere">
+    Geen van bovenstaande
+</label><br/>
+
+<br>
+<br>
+<label for="muziek">Wat is je favoriete muziekstijl?</label>
+<br>
+<label for="pop">
+    <input type="checkbox" id="pop">
+    Pop-muziek
+</label><br/>
+<label for="techno">
+    <input type="checkbox" id="techno">
+    Techno
+</label><br/>
+<label for="metal">
+    <input type="checkbox" id="metal">
+    Metal
+</label><br/>
+<label for="dubstep">
+    <input type="checkbox" id="dupstep">
+    Dupstep
+</label><br/>
+<label for="drumandbass">
+    <input type="checkbox" id="drumandbass">
+    Drum and bass
+</label><br/>
+<label for="andere">
+    <input type="checkbox" id="andere">
+    Nog iets anders
+</label><br/>
+
+<br>
+<br>
+
+<div class="form__field">
+					<input type="submit" value="Aanmelden" class="btn-aanmelden">	
+					
+				</div>
+
+
+
+</body>
+</html>
