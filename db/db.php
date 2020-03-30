88fd84bd8d80bd5622857edab426c3ccb683156d
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-03-30 23:40:23 +0200

    Update db.php

--- db/db.php
+++ db/db.php
@@ -6,7 +6,7 @@ class Db{
  
     public static function getConnection(){
         
-        include_once(__DIR__ . "../settings/settings.php");
+        include_once(__DIR__ . "/../settings/settings.php");
 
         if(self::$conn === null){
             self::$conn = new PDO("mysql:host=". SETTINGS ['db']['host']. ";dbname=" . SETTINGS ['db']['db'],SETTINGS['db']['user'] , SETTINGS['db']['password']);
