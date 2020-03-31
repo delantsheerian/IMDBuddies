5fea646efa39da7dfdc62bc081a6e8331ec2f090
delantsheerian <delantsheerian@gmail.com>
2020-03-31 14:19:50 +0200

    Added styling

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     include_once(__DIR__ . "/classes/user.php");
 
 	if (!empty($_POST)) {
+
         $email = $_POST['email'];
         $password = $_POST['wachtwoord'];
     
@@ -11,8 +12,13 @@
         $login->setEmail($email);
         $login->setPassword($password);
         $login->canLogin();
+
         if (!$login->canLogin()) {
             $error = "Er liep iets fout.";
+        }
+
+        else {
+            header('Location: index.php');
         }
     }
 
@@ -23,34 +29,47 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<title>Meld je aan bij Companion</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Aanmelden | Companion</title>
 </head>
 <body>
+    
     <?php if(isset($error)): ?>
-    <div class="error" style="color: red;"><?php echo $error; ?></div>
+        <!--<div class="error" style="color: red;"><?php echo $error; ?></div>-->
+        <?php endif; ?>
+
+        <?php if(isset($success)): ?>
+        <!--<div class="success" style="color: green;"><?php echo $success; ?></div>-->
     <?php endif; ?>
 
-    <?php if(isset($success)): ?>
-    <div class="success" style="color: green;"><?php echo $success; ?></div>
-    <?php endif; ?>
+    <div id="banner"></div>
 
-    <form action="" method="post">
+    <div id="geen_lid">
+        <p>Nog geen Companion? <a href="register.php">Registreer hier</a></p>
+    </div>
+    
+    <div id="form">
 
-        <div>
-            <label for="email">Studenten Email</label>
-            <input type="text" name="email" id="email">
-		</div>
-		
-		<div>
-            <label for="wachtwoord">Wachtwoord</label>
-            <input type="password" name="wachtwoord" id="wachtwoord">
-        </div>
-        
-        <div>
-			<input type="submit" value="Sign me up">
-		</div>
-        
-        </form>
+        <form class="aanmeldenBuddy" action="" method="post">
+
+            <div id="form_title"><h2>Meld je aan bij Companion</h2>
+
+            <div>
+                <label for="email">Studenten Email</label>
+                <input class="input" type="text" name="email" id="email" required>
+            </div>
+            
+            <div id="password_field">
+                <label for="wachtwoord">Wachtwoord</label>
+                <input class="input" type="password" name="wachtwoord" id="wachtwoord" required>
+            </div>
+            
+            <div>
+                <input class="btn-aanmelden" type="submit" value="Aanmelden">
+            </div>
+            
+            </form>
+    </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,9 +1,7 @@
-
 <?php
 
 	include_once (__DIR__ . "/db/db.php");
 	include_once(__DIR__ . "/classes/User.php");
-	
 	
 	if (!empty($_POST)){	
 
@@ -13,28 +11,20 @@
 		$achternaam = $_POST['achternaam'];
 		$wachtwoord = $_POST['wachtwoord'];
 
-	}
-
-	if(!empty($_POST)){
-		
-
 		try{
 			
 			$user = new User();
 			
-					//wachtwoord beveiligen
+			//wachtwoord beveiligen
 			$wachtwoord = password_hash($wachtwoord, PASSWORD_DEFAULT, ['cost' => 12]); //2 tot de zoveelste keer gehasht
-			
 			
 			$user->setEmail($_POST['email']);
 			$user->setVoornaam($_POST['voornaam']);
 			$user->setAchternaam($_POST['achternaam']);
 			$user->setPassword($wachtwoord);
 
-
-		
 			$user->save();
-			$succes="user saved";
+			$succes = "user saved";
 		}
 
 		catch (\Throwable $th){
@@ -52,58 +42,68 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="UTF-8">
-  <title>Aanmelden bij Companion</title>
+  <title>Registreren | Companion</title>
 </head>
 
 <body>
-	<div class="aanmeldenBuddy">
-		<div class="form aanmeldenBuddy">
-			<form action="" method="post">
 
-				<h2 form__title>Registreren</h2>
-				<div>
+	<div id="banner"></div>
 
-				<?php if(isset($error)) : ?>
+	<div id="lid">
+        <p>Al reeds een Companion? <a href="login.php">Log je hier in</a></p>
+    </div>
+
+	<div id="form">
+
+		<form class="registrerenBuddy" action="" method="post">
+
+			<h2 id="form__title">Registreren bij Companion</h2>
+			<div>
+
+			<?php if(isset($error)) : ?>
 
 				<div class="formerror">
 					<p><?php echo $error ?></p>
+				</div>
+
+				<div class="formsucces">
+					<p><?php echo $succes ?></p>
 				</div>
 
 				<?php endif; ?>
 
 				<div class="form__field">
 					<label for="Email">Email</label>
-					<input type="text" id="Email" name="email">
+					<input type="text" class="input" name="email">
 				</div>
 				<br>
 
 				<div class="form__field">
 					<label for="Voornaam">Voornaam</label>
-					<input type="text" id="Voornaam" name="voornaam">
+					<input type="text" class="input" name="voornaam">
 				</div>
                 <br>
 
                 <div class="form__field">
 					<label for="Achernaam">Achternaam</label>
-					<input type="text" id="Achternaam" name="achternaam">
+					<input type="text" class="input" name="achternaam">
 				</div>
                 <br>
                 
 				<div class="form__field">
 					<label for="Wachtwoord">Wachtwoord</label>
-					<input type="password" id="Wachtwoord" name="wachtwoord">
+					<input type="password" class="input" name="wachtwoord">
 				</div>
                 <br>
 
 				<div class="form__field">
-					<input type="submit" value="Aanmelden" class="btn-aanmelden">	
+					<input type="submit" value="Creëer account" class="btn-aanmelden">	
 				</div>
 
-			</form>
-		</div>
+		</form>
 	</div>
 </body>
 </html>
